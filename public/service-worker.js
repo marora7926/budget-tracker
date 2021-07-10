cfee567d0f911c275bd1205e6c5cd0f3b47edfac
Mohit Arora <mohit.arora@sydney.edu.au>
2021-07-10 18:51:46 +1000

    added db file

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,14 +2,14 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/assets/css/styles.css",
-    "/assets/js/index.js",
+    "/assets/js/server.js",
     "/assets/js/db.js",
     "/assets/images/icons/icon-192x192.png",
     "/assets/images/icons/icon-512x512.png",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
-  
+
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
   
@@ -64,7 +64,6 @@ self.addEventListener("fetch", (evt) => {
         // stop execution of the fetch event callback
         return;
     }
-  
     // if the request is not for the API, serve static assets using
     evt.respondWith(
         caches.match(evt.request).then((response) => {

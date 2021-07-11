a91493490ae366deae0656ecbda234c9a70c8e80
Mohit Arora <mohit.arora@sydney.edu.au>
2021-07-11 20:45:21 +1000

    resized given icons and addeed new icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,6 +7,8 @@ const FILES_TO_CACHE = [
     "./assets/css/styles.css",
     "./assets/js/index.js",
     "./assets/js/db.js",
+    "./assets/images/icons/icon-128x128.png",
+    "./assets/images/icons/icon-144x144.png",
     "./assets/images/icons/icon-192x192.png",
     "./assets/images/icons/icon-512x512.png",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
